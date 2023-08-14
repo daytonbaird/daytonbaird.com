2269790aeaa16ab815ba0f281e94672324487f70
Dayton Baird <dayton@daytonbaird.com>
2023-08-14 02:25:57 -0700

    feat: update experiences section

--- src/constants/index.js
+++ src/constants/index.js
@@ -20,6 +20,7 @@ import {
   jobit,
   tripguide,
   threejs,
+  sneakydeals,
 } from '../assets';
 
 export const navLinks = [
@@ -133,10 +134,69 @@ const technologies = [
 
 const experiences = [
   {
+    title: 'Software Engineer',
+    company_name: 'Altice USA (Juice Media)',
+    icon: altice,
+    iconBg: '#000000',
+    date: 'Feb 2022 - Present',
+    points: [
+      `Led team of 5 full stack engineers in the development of flagship BI platform using React, Redux, Next.js,
+      TypeScript to bring in $12M in additional company revenue and save consumer analysis time by 80%`,
+      `Built 2 Python REST APIs using Flask for client data consumption and aggregation atop AWS Redshift and
+      PostgreSQL deployed via Docker and AWS Fargate`,
+      `Standardized agile project workflow as team size grew 5x through company acquisition, leading sprints and daily
+      scrums for end-to-end delivery, speeding time-to-market upwards of 50%`,
+      `Architected two major refactors for core services, increasing speed performance by a measured 74%`,
+      `Coordinated with data science, data engineering and business intelligence teams to deliver data solutions using
+      SQL, Python, TypeScript for powering in-house machine learning models behind $190M+ advertising campaigns`,
+      `Overhauled high-scale ETL data ingest solution for 100s of terabytes of ad server and client pixel data built with Python, SQL and TypeScript using AWS CloudFront, Kinesis, S3, Lambda, Redshift, RDS and DBT`,
+    ],
+  },
+  {
+    title: 'Full Stack Engineer',
+    company_name: 'Cleara Inc',
+    icon: cleara,
+    iconBg: '#dcd4d4',
+    date: 'Aug 2021 - Sep 2022',
+    points: [
+      `Spearheaded development of flagship mobile app written in TypeScript using React Native, Expo, GraphQL, Apollo, PostgreSQL and the Amazon Selling Partner APIs for analyzing 1000s of retail products per minute`,
+      `Illustrated a responsive, mobile-first UI design for the mobile application and web app using Figma`,
+      `Served as product owner and subject matter expert, mediating communication and requirements between all
+      involved technical teams and non-technical cohorts`,
+      `Led product discovery efforts, including user interviews, user testing, and market research for over 40 stakeholders`,
+    ],
+  },
+  {
+    title: 'Full Stack Engineer',
+    company_name: 'ASIN Central',
+    icon: asincentral,
+    iconBg: '#17140f',
+    date: 'Jan 2021 - Aug 2022',
+    points: [
+      `Architected and delivered the company's flagship product, an automated interactive bot for use by Amazon sellers, written in JavaScript using Node.js, Express, MongoDB and Docker`,
+      `Maintained 13 unique ecommerce product analysis tools for use by by over 300+ daily active Amazon Sellers,
+      written in JavaScript using Node, Express, MongoDB, and Docker`,
+      `Designed, developed, and deployed 3 web apps using React, Redux, JavaScript, and HTML/CSS`,
+      `Led social media marketing efforts and automation processes for the sales funnel`,
+    ],
+  },
+  {
+    title: 'Front End Web Developer',
+    company_name: 'Sneaky Deals LLC',
+    icon: sneakydeals,
+    iconBg: '#ffffff',
+    date: 'Jan 2020 - Jan 2021',
+    points: [
+      `Served as end-to-end designer and sole developer for a myriad of e-commerce brands, increasing sales 2x-3x through efficient CRO-forward design and development`,
+      `Developed and maintained 10+ e-commerce websites, including a custom Shopify theme using Shopify Liquid, JavaScript, and CSS`,
+      `Launched 3 e-commerce brands from ideation to launch, including brand identity, product sourcing, and website development`,
+    ],
+  },
+  {
     title: 'Corporate Trainer',
     company_name: 'QuikTrip',
     icon: quiktrip,
-    iconBg: '#383E56',
+    iconBg: '#cf152d',
     date: 'July 2016 - March 2021',
     points: [
       `Built React Native mobile app for QuikTrip's training team, written in JavaScript using the React
@@ -145,44 +205,6 @@ const experiences = [
       non-technical documentation (60+ documents)`,
       `Worked closely with key stakeholders on optimizing design, increasing employee retention averages by 20%`,
       `Awarded the "Best of the Best" award for leadership of the highest-performing QuikTrip team`,
-    ],
-  },
-  {
-    title: 'Front End Web Developer',
-    company_name: 'Sneaky Deals LLC',
-    icon: quiktrip,
-    iconBg: '#E6DEDD',
-    date: 'Jan 2021 - Feb 2022',
-    points: [
-      `Served as end-to-end designer and sole developer for a myriad of e-commerce brands, increasing sales 2x-3x through efficient CRO-forward design and development`,
-      `Developed and maintained 10+ e-commerce websites, including a custom Shopify theme using Shopify Liquid, JavaScript, and CSS`,
-      `Launched 3 e-commerce brands from ideation to launch, including brand identity, product sourcing, and website development`,
-    ],
-  },
-  {
-    title: 'Web Developer',
-    company_name: 'Shopify',
-    icon: quiktrip,
-    iconBg: '#383E56',
-    date: 'Jan 2022 - Jan 2023',
-    points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.',
-    ],
-  },
-  {
-    title: 'Full stack Developer',
-    company_name: 'Meta',
-    icon: quiktrip,
-    iconBg: '#E6DEDD',
-    date: 'Jan 2023 - Present',
-    points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.',
     ],
   },
 ];
